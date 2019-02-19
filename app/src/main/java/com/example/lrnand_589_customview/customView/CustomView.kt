c1d87af938ae2111c063d2eb9f165eca0226aaa6
maxdoctorkurt@mail.ru <querty123>
2019-02-19 10:39:34 +0300

    рефакторинг

--- app/src/main/java/com/example/lrnand_589_customview/customView/CustomView.kt
+++ app/src/main/java/com/example/lrnand_589_customview/customView/CustomView.kt
@@ -17,43 +17,104 @@ import com.example.lrnand_589_customview.R
 class CustomView(context: Context, private var attrSet: AttributeSet) :
     FrameLayout(context, attrSet) {
 
+    var colorSet = mutableListOf<Int>()
+    var limitExceededCallback: (() -> Unit)? = null
     @ColorInt
     private val defaultColor: Int = Color.GREEN // если не указали в атрибутах свой цвет то рисуем этим
     private val maxFigures: Int = 10
     private var figures = mutableListOf<Figure>()
-    var colorSet = mutableListOf<Int>()
-    var limitExceededCallback: (() -> Unit)? = null
+
     init {
         setWillNotDraw(false)
-        isSaveEnabled = true;
+        obtainAttributes()
+    }
 
-        // TODO - посмотреть что там со ссылками на ресурсы
-        context.theme.obtainStyledAttributes(
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val x: Float = event.x
+        val y: Float = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                addFigure(x, y)
+            }
+        }
+
+        return true
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        canvas?.let {
+            drawAll(canvas)
+        }
+    }
+
+    override fun onSaveInstanceState(): Parcelable? {
+        val superState = super.onSaveInstanceState()
+
+        if (superState != null) {
+            val ss = SavedState(superState)
+            val gson = Gson()
+            ss.figuresAndColors = gson.toJson(FiguresAndColors(figures, colorSet))
+            return ss
+        }
+
+        return superState
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable) {
+        if (state !is SavedState) {
+            super.onRestoreInstanceState(state)
+            return
+        }
+
+        val json = state.figuresAndColors
+        val gson = Gson()
+        val figuresAndColors = gson.fromJson(json, FiguresAndColors::class.java)
+
+        this.colorSet = figuresAndColors.colors
+        this.figures = figuresAndColors.figures
+
+        super.onRestoreInstanceState(state.superState)
+    }
+
+    private fun obtainAttributes() {
+        val typedArray = context.obtainStyledAttributes(
             attrSet,
             R.styleable.CustomView,
             0, 0
-        ).apply {
+        )
 
-            try {
-                val colors = getTextArray(R.styleable.CustomView_colorSetReference)
-                colors?.let {
-                    val s = it.forEach {
-                        val s = it.toString()
-                        colorSet.add(Color.parseColor(s))
-                    }
-                }
-            } finally {
-                recycle()
+        val colors = typedArray.getTextArray(R.styleable.CustomView_colorSetReference)
+
+        colors?.let {
+            it.forEach { color ->
+                val colorStr = color.toString()
+                colorSet.add(Color.parseColor(colorStr))
             }
         }
+
+        typedArray.recycle()
     }
 
     private fun addFigure(x: Float, y: Float) {
         val paint = Paint()
+
         paint.color = if (colorSet.isNotEmpty()) colorSet.random() else defaultColor
         paint.flags = Paint.ANTI_ALIAS_FLAG
+
         figures.add(Figure.withRandomBoundsAndType(x, y, paint))
+
         invalidate()
+    }
+
+    private fun drawFiguresCount(canvas: Canvas) {
+        val text = figures.size.toString()
+        val paint = Paint()
+
+        paint.color = Color.BLACK
+        paint.textSize = 72f
+
+        canvas.drawText(text, 25f, 100f, paint)
     }
 
     private fun drawAll(canvas: Canvas) {
@@ -67,62 +128,16 @@ class CustomView(context: Context, private var attrSet: AttributeSet) :
             limitExceededCallback?.invoke()
         }
 
-        // вывести количество фигур
-        val text = figures.size.toString()
-        val paint = Paint()
-        paint.color = Color.BLACK
-        paint.textSize = 72f
-        canvas.drawText(text, 25f, 100f, paint)
-    }
-
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        val x: Float = event.x
-        val y: Float = event.y
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                addFigure(x, y)
-            }
-        }
-        return true
-    }
-
-    override fun onDraw(canvas: Canvas?) {
-        canvas?.let {
-            drawAll(canvas)
-        }
-    }
-
-    public override fun onSaveInstanceState(): Parcelable? {
-        val superState = super.onSaveInstanceState()
-
-        if (superState != null) {
-            val ss = SavedState(superState)
-            val gson = Gson()
-            ss.figuresAndColors = gson.toJson(FiguresAndColors(figures, colorSet))
-            return ss
-        }
-        return superState
-    }
-
-    public override fun onRestoreInstanceState(state: Parcelable) {
-        if (state !is SavedState) {
-            super.onRestoreInstanceState(state)
-            return
-        }
-
-        val json = state.figuresAndColors
-        val gson = Gson()
-        val figuresAndColors = gson.fromJson(json, FiguresAndColors::class.java)
-        this.colorSet = figuresAndColors.colors
-        this.figures = figuresAndColors.figures
-        super.onRestoreInstanceState(state.superState)
+        drawFiguresCount(canvas)
     }
 
     data class FiguresAndColors(val figures: MutableList<Figure>, val colors: MutableList<Int>)
 
     internal class SavedState : View.BaseSavedState {
         lateinit var figuresAndColors: String
+
         constructor(superState: Parcelable) : super(superState) {}
+
         private constructor(parcel: Parcel) : super(parcel) {
             val s = parcel.readString()
             s?.let {
@@ -143,6 +158,7 @@ class CustomView(context: Context, private var attrSet: AttributeSet) :
             override fun createFromParcel(parcel: Parcel): SavedState {
                 return SavedState(parcel)
             }
+
             override fun newArray(size: Int): Array<SavedState?> {
                 return arrayOfNulls(size)
             }
--- app/src/main/java/com/example/lrnand_589_customview/customView/Figure.kt
+++ app/src/main/java/com/example/lrnand_589_customview/customView/Figure.kt
@@ -12,11 +12,20 @@ class Figure(
     private val type: Type,
     private val paint: Paint
 ) {
+    fun drawSelf(canvas: Canvas) {
+        when (type) {
+            Type.SQUARE ->
+                canvas.drawRect(getSquareF(), paint)
 
-    enum class Type {
-        ROUND,
-        SQUARE,
-        ROUNDED_SQUARE
+            Type.ROUNDED_SQUARE -> {
+                drawCross(canvas)
+                drawFourCircles(canvas)
+            }
+
+            Type.ROUND -> {
+                canvas.drawCircle(x, y, size / 2, paint)
+            }
+        }
     }
 
     private fun getHalfSize(): Float {
@@ -24,8 +33,11 @@ class Figure(
     }
 
     private fun getLeft(): Float = x - getHalfSize()
+
     private fun getTop(): Float = y + getHalfSize()
+
     private fun getRight(): Float = x + getHalfSize()
+
     private fun getBottom(): Float = y - getHalfSize()
 
     private fun getSquareF(): RectF {
@@ -36,53 +48,44 @@ class Figure(
         return Rect(getLeft().toInt(), getTop().toInt(), getRight().toInt(), getBottom().toInt())
     }
 
-    // Должно уметь рисовать круг, квадрат, квадрат с закругленными краями.
-    fun drawSelf(canvas: Canvas) {
+    private fun drawCross(canvas: Canvas) {
+        val sizePart = size / 4
 
-        when (type) {
+        val verticalStrip = getSquareF()
+        verticalStrip.left = verticalStrip.left + sizePart
+        verticalStrip.right = verticalStrip.right - sizePart
 
-            Type.SQUARE ->
-                canvas.drawRect(getSquareF(), paint)
+        val horizontalStrip = getSquareF()
+        horizontalStrip.top = horizontalStrip.top - sizePart
+        horizontalStrip.bottom = horizontalStrip.bottom + sizePart
 
-            Type.ROUNDED_SQUARE -> {
+        canvas.drawRect(verticalStrip, paint)
+        canvas.drawRect(horizontalStrip, paint)
+    }
 
-                // рисуем как крест ...
-                val sizePart = size / 4
+    private fun drawFourCircles(canvas: Canvas) {
+        val sizePart = size / 4
 
-                val verticalStrip = getSquareF()
-                verticalStrip.left = verticalStrip.left + sizePart
-                verticalStrip.right = verticalStrip.right - sizePart
+        val cx1 = x - sizePart
+        val cx2 = x - sizePart
+        val cx3 = x + sizePart
+        val cx4 = x + sizePart
 
-                val horizontalStrip = getSquareF()
-                horizontalStrip.top = horizontalStrip.top - sizePart
-                horizontalStrip.bottom = horizontalStrip.bottom + sizePart
+        val cy1 = y - sizePart
+        val cy2 = y + sizePart
+        val cy3 = y - sizePart
+        val cy4 = y + sizePart
 
-                canvas.drawRect(verticalStrip, paint)
-                canvas.drawRect(horizontalStrip, paint)
+        canvas.drawCircle(cx1, cy1, sizePart, paint)
+        canvas.drawCircle(cx2, cy2, sizePart, paint)
+        canvas.drawCircle(cx3, cy3, sizePart, paint)
+        canvas.drawCircle(cx4, cy4, sizePart, paint)
+    }
 
-                // ... и четыре круга
-                val cx1 = x - sizePart
-                val cx2 = x - sizePart
-                val cx3 = x + sizePart
-                val cx4 = x + sizePart
-
-                val cy1 = y - sizePart
-                val cy2 = y + sizePart
-                val cy3 = y - sizePart
-                val cy4 = y + sizePart
-
-                canvas.drawCircle(cx1, cy1, sizePart, paint)
-                canvas.drawCircle(cx2, cy2, sizePart, paint)
-                canvas.drawCircle(cx3, cy3, sizePart, paint)
-                canvas.drawCircle(cx4, cy4, sizePart, paint)
-
-            }
-
-            Type.ROUND -> {
-                canvas.drawCircle(x, y, size / 2, paint)
-            }
-        }
-
+    enum class Type {
+        ROUND,
+        SQUARE,
+        ROUNDED_SQUARE
     }
 
     companion object {
